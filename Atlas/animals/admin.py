3dc0fbfa85a023478f3f30849c47dccd8c45c275
bboy19r20 <59454817+bboy19r20@users.noreply.github.com>
2020-01-03 17:33:07 +0100

    ok

--- Atlas/animals/admin.py
+++ Atlas/animals/admin.py
@@ -1,15 +1,37 @@
 from django.contrib import admin
 from .models import Animal
 from .models import Food
+from .models import Continent
+from .models import Type
+from .models import Environment
+from .models import Climate
 
 
 class AnimalAdmin(admin.ModelAdmin):
     list_display = ('name', 'eat')
 
-
 class FoodAdmin(admin.ModelAdmin):
     list_display = ['type']
 
+class ContinentAdmin(admin.ModelAdmin):
+    list_display = ['type']
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['type']
+
+class EnvironmentAdmin(admin.ModelAdmin):
+    list_display = ['type']
+
+class ClimateAdmin(admin.ModelAdmin):
+    list_display = ['type']
+
+
+
+
 admin.site.register(Food, FoodAdmin)
 admin.site.register(Animal, AnimalAdmin)
+admin.site.register(Continent, ContinentAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Environment, EnvironmentAdmin)
+admin.site.register(Climate, ClimateAdmin)
 # Register your models here.
--- Atlas/animals/models.py
+++ Atlas/animals/models.py
@@ -5,14 +5,35 @@ class Food(models.Model):
     def __str__(self):
         return self.type
 
+class Continent(models.Model):
+    type = models.CharField(max_length=100)
+    def __str__(self):
+        return self.type
+
+class Type(models.Model):
+    type = models.CharField(max_length=100)
+    def __str__(self):
+        return self.type
+
+class Environment(models.Model):
+    type = models.CharField(max_length=100)
+    def __str__(self):
+        return self.type
+
+class Climate(models.Model):
+    type = models.CharField(max_length=100)
+    def __str__(self):
+        return self.type
+
+
+
 
 class Animal(models.Model):
     name = models.CharField(max_length=150)
     eat = models.ForeignKey(Food, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    environment = models.ForeignKey(Environment, on_delete=models.CASCADE)
     image_url = models.CharField(max_length=2083)
     objects: models.Manager()
 
 
-
-
-# Create your models here.
